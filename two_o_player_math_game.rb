4a3915612b8c2034d9eab3d4db4e70245124f778
Jaewon Bae <jaey.pro@gmail.com>
2023-04-05 23:49:23 -0400

    initial commit

---
+++ two_o_player_math_game.rb
@@ -0,0 +1,7 @@
+#Nouns: Player (score, questions)
+#Roles: holds player blueprints, holds score, methods for the questions
+
+#Scores: value starting at 3
+#Subtract_Score: subtract 1 from score when the player gets the question wrong
+
+#Player1 will contain the initla game loop
